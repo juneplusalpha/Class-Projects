eeb1ab7353657bdbf5eee46228b84a617335633e
June Park <juneplusalpha@icloud.com>
2018-03-26 13:51:23 -0400

    javascript example added

---
+++ JavaScript Practice/lab2_js_data.js
@@ -0,0 +1,239 @@
+/**
+ * Created by heslicia on 1/12/2018.
+ */
+var mvData = [
+    {
+        "Title": "The Land Girls",
+        "Worldwide Gross": 146083,
+        "Production Budget": 8000000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "First Love, Last Rites",
+        "Worldwide Gross": 10876,
+        "Production Budget": 300000,
+        "Major Genre": "Drama"
+    },
+    {
+        "Title": "I Married a Strange Person",
+        "Worldwide Gross": 203134,
+        "Production Budget": 250000,
+        "Major Genre": "Comedy"
+    },
+    {
+        "Title": "Let's Talk About Sex",
+        "Worldwide Gross": 373615,
+        "Production Budget": 300000,
+        "Major Genre": "Comedy"
+    },
+    {
+        "Title": "Slam",
+        "Worldwide Gross": 1087521,
+        "Production Budget": 1000000,
+        "Major Genre": "Drama"
+    },
+    {
+        "Title": "Mississippi Mermaid",
+        "Worldwide Gross": 2624551,
+        "Production Budget": 1600000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Following",
+        "Worldwide Gross": 44705,
+        "Production Budget": 6000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Foolish",
+        "Worldwide Gross": 6026908,
+        "Production Budget": 1600000,
+        "Major Genre": "Comedy"
+    },
+    {
+        "Title": "Pirates",
+        "Worldwide Gross": 6341825,
+        "Production Budget": 40000000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Duel in the Sun",
+        "Worldwide Gross": 20400000,
+        "Production Budget": 6000000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Tom Jones",
+        "Worldwide Gross": 37600000,
+        "Production Budget": 1000000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Oliver!",
+        "Worldwide Gross": 37402877,
+        "Production Budget": 10000000,
+        "Major Genre": "Musical"
+    },
+    {
+        "Title": "To Kill A Mockingbird",
+        "Worldwide Gross": 13129846,
+        "Production Budget": 2000000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Tora, Tora, Tora",
+        "Worldwide Gross": 29548291,
+        "Production Budget": 25000000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Hollywood Shuffle",
+        "Worldwide Gross": 5228617,
+        "Production Budget": 100000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Over the Hill to the Poorhouse",
+        "Worldwide Gross": 3000000,
+        "Production Budget": 100000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Wilson",
+        "Worldwide Gross": 2000000,
+        "Production Budget": 5200000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "Darling Lili",
+        "Worldwide Gross": 5000000,
+        "Production Budget": 22000000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "The Ten Commandments",
+        "Worldwide Gross": 80000000,
+        "Production Budget": 13500000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "12 Angry Men",
+        "Worldwide Gross": 0,
+        "Production Budget": 340000,
+        "Major Genre": "Drama"
+    },
+    {
+        "Title": "Twelve Monkeys",
+        "Worldwide Gross": 168841459,
+        "Production Budget": 29000000,
+        "Major Genre": "Drama"
+    },
+    {
+        "Title": 1776,
+        "Worldwide Gross": 0,
+        "Production Budget": 4000000,
+        "Major Genre": "Drama"
+    },
+    {
+        "Title": 1941,
+        "Worldwide Gross": 94875000,
+        "Production Budget": 32000000,
+        "Major Genre": "Comedy"
+    },
+    {
+        "Title": "Chacun sa nuit",
+        "Worldwide Gross": 18435,
+        "Production Budget": 1900000,
+        "Major Genre": "Thriller/Suspense"
+    },
+    {
+        "Title": "2001: A Space Odyssey",
+        "Worldwide Gross": 68700000,
+        "Production Budget": 10500000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "20,000 Leagues Under the Sea",
+        "Worldwide Gross": 28200000,
+        "Production Budget": 5000000,
+        "Major Genre": "Adventure"
+    },
+    {
+        "Title": "20,000 Leagues Under the Sea",
+        "Worldwide Gross": 8000000,
+        "Production Budget": 200000,
+        "Major Genre": "Adventure"
+    },
+    {
+        "Title": "24 7: Twenty Four Seven",
+        "Worldwide Gross": 72544,
+        "Production Budget": 2000000,
+        "Major Genre": "Comedy"
+    },
+    {
+        "Title": "Twin Falls Idaho",
+        "Worldwide Gross": 1027228,
+        "Production Budget": 500000,
+        "Major Genre": "Drama"
+    },
+    {
+        "Title": "Three Kingdoms: Resurrection of the Dragon",
+        "Worldwide Gross": 22139590,
+        "Production Budget": 20000000,
+        "Major Genre": "Action"
+    },
+    {
+        "Title": "3 Men and a Baby",
+        "Worldwide Gross": 167780960,
+        "Production Budget": 15000000,
+        "Major Genre": "Comedy"
+    },
+    {
+        "Title": "3 Ninjas Kick Back",
+        "Worldwide Gross": 11744960,
+        "Production Budget": 20000000,
+        "Major Genre": "Action"
+    },
+    {
+        "Title": "Forty Shades of Blue",
+        "Worldwide Gross": 172569,
+        "Production Budget": 1500000,
+        "Major Genre": "Drama"
+    },
+    {
+        "Title": "42nd Street",
+        "Worldwide Gross": 2300000,
+        "Production Budget": 439000,
+        "Major Genre": "Musical"
+    },
+    {
+        "Title": "Four Rooms",
+        "Worldwide Gross": 4301000,
+        "Production Budget": 4000000,
+        "Major Genre": "Comedy"
+    },
+    {
+        "Title": "The Four Seasons",
+        "Worldwide Gross": 42488161,
+        "Production Budget": 6500000,
+        "Major Genre": "Comedy"
+    },
+    {
+        "Title": "Four Weddings and a Funeral",
+        "Worldwide Gross": 242895809,
+        "Production Budget": 4500000,
+        "Major Genre": "Romantic Comedy"
+    },
+    {
+        "Title": "51 Birch Street",
+        "Worldwide Gross": 84689,
+        "Production Budget": 350000,
+        "Major Genre": ""
+    },
+    {
+        "Title": "55 Days at Peking",
+        "Worldwide Gross": 10000000,
+        "Production Budget": 17000000,
+        "Major Genre": "Drama"
+    }
+];
